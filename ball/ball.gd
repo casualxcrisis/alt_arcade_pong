c98e715d85e3cde16b45fd70a1304a0d2b829c17
noah <harrington.noah@tutamail.com>
2026-09-16 12:17:49 -0600

    Added a countdown UI element and timer delay for serving of the pong ball.

--- ball/ball.gd
+++ ball/ball.gd
@@ -3,9 +3,21 @@ extends CharacterBody2D
 @export var ball_speed = 12
 
 var array: Array[int] = [1, 2, 3, -1, -2, -3] # sets the array to choose a random number from to vary the serve angle
-var other_array: Array[Variant] = [-ball_speed, ball_speed]
+var other_array: Array[Variant] = [-ball_speed, ball_speed] # sets the array to choose either a left-side serve or right-side serve
 
 func _ready() -> void:
+	get_node("AnimatedSprite2D").visible = false
+	get_node("%countdown_numbers").visible = true
+	get_node("%countdown_numbers").set_text("3")
+	await get_tree().create_timer(1).timeout
+	get_node("%countdown_numbers").set_text("2")
+	await get_tree().create_timer(1).timeout
+	get_node("%countdown_numbers").set_text("1")
+	await get_tree().create_timer(1).timeout
+	get_node("%countdown_numbers").set_text("GRIP!")
+	await get_tree().create_timer(1).timeout
+	get_node("%countdown_numbers").visible = false
+	get_node("AnimatedSprite2D").visible = true
 	velocity = Vector2(other_array.pick_random(), array.pick_random()) # this will launch the ball to the left, maybe add an conditional that flips it if the ball has collided with other side - to simulate the serve from winning player
 	
 func _process(_delta: float) -> void:
@@ -17,4 +29,5 @@ func _physics_process(_delta: float) -> void:
 	if collision:
 		var normal := collision.get_normal()
 		velocity = velocity.bounce(normal)
+		
 			
--- levels/right_score.gd
+++ levels/right_score.gd
@@ -9,3 +9,4 @@ func _on_right_score_zone_body_entered(body: Node2D) -> void:
 	right_score += 1
 	var string = var_to_str(right_score)
 	%right_score.set_text("Score: " + string)
+	
