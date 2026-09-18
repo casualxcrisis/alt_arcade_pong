b589375c438d2c97f6473bcbc3f7fde999c47fa3
noah <harrington.noah@tutamail.com>
2026-09-18 17:08:38 -0600

    Added grow function for both sprite and collision on player paddles.

--- player_paddles/player_left.gd
+++ player_paddles/player_left.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+@onready var base_sprite_scale_y = $Sprite2D.scale.y
+@onready var base_sprite_scale_x = $Sprite2D.scale.x
+@onready var base_coll_scale_y = $CollisionShape2D.scale.y
 
 func getYDir() -> float:
 	return Input.get_action_strength("p_left_down") - Input.get_action_strength("p_left_up")
 	
+	
+		
 	
 func _process(_delta: float) -> void:
 	var dir :Vector2=Vector2(0, getYDir())
@@ -13,5 +18,13 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("p_left_down"):
 		$AudioStreamPlayer2D.play()
 	elif Input.is_action_just_pressed("p_left_up"):
-		$AudioStreamPlayer2D.play()
+		$AudioStreamPlayer2D.play()		
+	elif Input.is_action_just_pressed("grow"):
+		$Sprite2D.scale.y = base_sprite_scale_y * 5
+		$Sprite2D.scale.x = base_sprite_scale_x * 5
+		$CollisionShape2D.scale.y = base_coll_scale_y * 5
+	elif Input.is_action_just_released("grow"):
+		$Sprite2D.scale.y = base_sprite_scale_y
+		$Sprite2D.scale.x = base_sprite_scale_x
+		$CollisionShape2D.scale.y = base_coll_scale_y
 	move_and_slide()
--- player_paddles/player_right.gd
+++ player_paddles/player_right.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+@onready var base_sprite_scale_y = $player_right_sprite.scale.y
+@onready var base_sprite_scale_x = $player_right_sprite.scale.x
+@onready var base_coll_scale_y = $player_right_collision.scale.y
+
+
+
 func getYDir() -> float:
 	return Input.get_action_strength("p_right_down") - Input.get_action_strength("p_right_up")
 	
@@ -13,5 +19,13 @@ func _process(_delta: float) -> void:
 		$AudioStreamPlayer2D.play()
 	elif Input.is_action_just_pressed("p_right_up"):
 		$AudioStreamPlayer2D.play()
+	elif Input.is_action_just_pressed("grow_right"):
+		$player_right_sprite.scale.y = base_sprite_scale_y * 5
+		$player_right_sprite.scale.x = base_sprite_scale_x * 5
+		$player_right_collision.scale.y = base_coll_scale_y * 5
+	elif Input.is_action_just_released("grow_right"):
+		$player_right_sprite.scale.y = base_sprite_scale_y
+		$player_right_sprite.scale.x = base_sprite_scale_x
+		$player_right_collision.scale.y = base_coll_scale_y	
 	move_and_slide()
 	
